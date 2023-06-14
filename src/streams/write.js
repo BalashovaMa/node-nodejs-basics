1a421d156d22736b5dfa0b1f06b6900322f42ea1
BalashovaMa <balashova.m.nd@gmail.com>
2023-06-14 16:10:50 +0300

    feat: add function write to streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = './src/streams/files/fileToWrite.txt';
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
+
+    process.stdin.on('end', () => {
+        writeStream.end();
+    });
+
+    await new Promise((resolve, reject) => {
+        writeStream.on('finish', () => {
+            resolve();
+        });
+
+        writeStream.on('error', (error) => {
+            reject(error);
+        });
+    });
+
+
 };
 
 await write();
