f6ee9151698c92d54e6791b70099bf6f64aba1da
BalashovaMa <balashova.m.nd@gmail.com>
2023-06-14 15:36:21 +0300

    feat: add read.js to streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('./src/streams/files/fileToRead.txt');
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk)
+    });
+
+    readStream.on('end', () => console.log('\nFile reading completed'));
+
+    readStream.on('error', (error) => console.error('Error reading file:', error));
 };
 
 await read();
