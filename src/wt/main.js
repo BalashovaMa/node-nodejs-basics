23c4411a39fc52b0dc3ecefdcbe1d0fd4b3e9c6e
BalashovaMa <balashova.m.nd@gmail.com>
2023-06-16 15:24:23 +0300

    feat: add functions for files main.js and worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const numCPUs = os.cpus().length;
+const workerFile = './src/wt/worker.js';
+
 const performCalculations = async () => {
-    // Write your code here
+    const results = [];
+
+    const createWorker = (n) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerFile);
+            worker.on('message', (result) => {
+                results.push({ status: 'resolved', data: result });
+                resolve();
+            });
+            worker.on('error', (error) => {
+                results.push({ status: 'error', data: null });
+                resolve();
+            });
+            worker.postMessage(n);
+        });
+    };
+
+    const start = 10;
+    const workerPromises = [];
+
+    for (let i = 0; i < numCPUs; i++) {
+        const n = start + i;
+        workerPromises.push(createWorker(n));
+    }
+
+    await Promise.all(workerPromises);
+
+    console.log(results);
 };
 
 await performCalculations();
